520f8020b27f1f14f6e704fb282b445284edc048
Max <allenmaxnichols@gmail.com>
2024-09-20 22:10:09 -0500

    add pdf.js and show on canvas

--- config/index.js
+++ config/index.js
@@ -1,13 +1,19 @@
 const fastify = require("fastify")();
 const fastifyView = require("@fastify/view");
+const fastifyStatic = require("@fastify/static");
+const path = require("node:path");
 
 const app = fastify;
 
-app.register(fastifyView, {
-  engine: {
-    ejs: require("ejs"),
-  },
-  templates: "./views",
-});
+app
+  .register(fastifyView, {
+    engine: {
+      ejs: require("ejs"),
+    },
+    templates: "./views",
+  })
+  .register(fastifyStatic, {
+    root: path.join(__dirname, "../public"),
+  });
 
 module.exports = { app };
--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const { app } = require("./config");
+const { PDFDocument, rgb } = require("pdf-lib");
 
 app.get("/", (req, reply) => {
   reply.view("index.ejs", { title: "Resume Builder" });
+});
+
+app.get("/resume", async (req, reply) => {
+  const pdf = await PDFDocument.create();
+
+  const page = pdf.addPage([612, 792]);
+
+  page.drawText("Hello World!", {
+    x: 50,
+    y: 700,
+    size: 50,
+    color: rgb(0, 0, 0),
+  });
+
+  const pdfBytes = await pdf.save();
+
+  reply.header("Content-Type", "application/pdf");
+  reply.send(pdfBytes);
 });
 
 app.listen({ port: 3000 }, (err) => {
