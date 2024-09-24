92f29b791e4739a9560454bd1768ab94f4527fc8
Max <allenmaxnichols@gmail.com>
2024-09-24 14:12:03 -0500

    add form body

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const fastify = require("fastify")();
 const fastifyView = require("@fastify/view");
 const fastifyStatic = require("@fastify/static");
+const fastifyFormbody = require("@fastify/formbody");
 const path = require("node:path");
 
 const app = fastify;
@@ -14,6 +15,7 @@ app
   })
   .register(fastifyStatic, {
     root: path.join(__dirname, "../public"),
-  });
+  })
+  .register(fastifyFormbody);
 
 module.exports = { app };
